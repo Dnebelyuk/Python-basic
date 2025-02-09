040751f5e26e943a2f6db3a7f66708414ada8c99
Dnebelyuk <nebelyuk0206@gmail.com>
2025-02-09 20:45:13 +0700

    +description

--- lecture4.py
+++ lecture4.py
@@ -1,3 +1,6 @@
+# Есть словарь original_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
+# Составить из него новый словарь, содержащий только те элементы, у которых значения > или = 3.
+
 original_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
 
 new_dict = {key: value for key, value in original_dict.items() if value >= 3}
